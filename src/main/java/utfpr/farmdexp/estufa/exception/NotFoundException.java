f03dcce502887f5ae9255b8d1f9a7e454f460da7
Japa <joaozinhory@hotmail.com>
2025-09-17 14:52:09 -0300

    Ambient

--- src/main/java/utfpr/farmdexp/estufa/exception/NotFoundException.java
+++ src/main/java/utfpr/farmdexp/estufa/exception/NotFoundException.java
@@ -1,3 +1,7 @@
-public class NotFoundException {
+package utfpr.farmdexp.estufa.exception;
 
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
