f664168d46d852326c5da3896e6c0a74e75a2abd
Luiz Feikes <33578169+Luligabi1@users.noreply.github.com>
2025-09-24 13:29:13 -0300

    Add auth endpoints

---
+++ src/main/java/utfpr/farmdexp/estufa/controllers/UserController.java
@@ -0,0 +1,17 @@
+package utfpr.farmdexp.estufa.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("/info")
+    public ResponseEntity<?> getUserInfo() {
+        return ResponseEntity.ok("User info retrieved successfully.");
+    }
+    
+}
