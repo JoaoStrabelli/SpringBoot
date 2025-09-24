5997452bf4c916169506731cfb0047c8d2976272
Luiz Feikes <33578169+Luligabi1@users.noreply.github.com>
2025-09-24 13:54:14 -0300

    Fix endpoints

--- src/main/java/utfpr/farmdexp/estufa/security/InterceptorConfig.java
+++ src/main/java/utfpr/farmdexp/estufa/security/InterceptorConfig.java
@@ -5,24 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Configuração do interceptor
- * <p>
- * Este código configura um interceptor personalizado para validar tokens JWT em
- * requisições HTTP. O interceptor é aplicado a todos os endpoints que correspondem
- * ao padrão "/api/**".
- * 
- */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
-    // Injeção do interceptor personalizado
     @Autowired
     private CognitoTokenValidationInterceptor cognitoTokenValidationInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(cognitoTokenValidationInterceptor)
-                .addPathPatterns("/api/**"); // Aplica o interceptor aos seus endpoints protegidos
+                .addPathPatterns("/api/**");
     }
 }
